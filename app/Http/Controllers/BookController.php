ff81737ab6b19d4a019724da37dbf8e8db3e7829
izza wildan ridhoni <izzawildan88@gmail.com>
2021-07-09 20:54:56 +0700

    auth management

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Gate;
 
 class BookController extends Controller
 {
+    public function __construct(){
+        // OTORISASI GATE
+        $this->middleware(function($request, $next){
+            if(Gate::allows('manage-books')) return $next($request);
+            abort(403, 'Anda tidak memiliki cukup hak akses');
+        });
+    }
     /**
      * Display a listing of the resource.
      *
@@ -46,6 +55,16 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        \Validator::make($request->all(), [
+            "title" => "required|min:5|max:200",
+            "description" => "required|min:20|max:1000",
+            "author" => "required|min:3|max:100",
+            "publisher" => "required|min:3|max:200",
+            "price" => "required|digits_between:0,10",
+            "stock" => "required|digits_between:0,10",
+            "cover" => "required"
+        ])->validate();
+        
         $new_book=new \App\Models\Book;
         $new_book->title = $request->get('title');
         $new_book->description = $request->get('description');
@@ -112,6 +131,19 @@ class BookController extends Controller
     public function update(Request $request, $id)
     {
         $book =\App\Models\Book::findOrfail($id);
+
+        \Validator::make($request->all(), [
+            "title" => "required|min:5|max:200",
+            "slug" => [
+            "required",
+            Rule::unique("books")->ignore($book->slug, "slug")
+            ],
+            "description" => "required|min:20|max:1000",
+            "author" => "required|min:3|max:100",
+            "publisher" => "required|min:3|max:200",
+            "price" => "required|digits_between:0,10",
+            "stock" => "required|digits_between:0,10",
+        ])->validate();
 
         $book->title = $request->get('title');
         $book->slug = $request->get('slug');
--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -18,32 +18,55 @@ Create Book
             class="shadow-sm p-3 bg-white">
             @csrf
             <label for="title">Title</label><br>
-            <input type="text" class="form-control" name="title" placeholder="book title"><br>
+            <input type="text" value="{{old('title')}}" class="form-control {{$errors->first('title') ? "is-invalid" : ""}} "name="title" placeholder="book title">
+            <div class="invalid-feedback">
+                {{$errors->first('title')}}
+            </div>
+            <br>
 
             <label for="cover">Cover</label>
-            <input type="file" class="form-control" name="cover"><br>
+            <input type="file" class="form-control {{$errors->first('cover') ? "is-invalid" : ""}} name="cover">
+            <div class="invalid-feedback">
+                {{$errors->first('cover')}}
+            </div>
+            <br>
             
             <label for="description">Description</label><br>
-            <textarea name="description" id="description" class="form-control" placeholder="Give a description about this book"></textarea>
+            <textarea name="description" id="description" class="form-control {{$errors->first('description') ? "is-invalid" : ""}} " placeholder="Give a description about this book">{{old('description')}}</textarea>
+            <div class="invalid-feedback">
+                {{$errors->first('description')}}
+            </div>
             <br>
 
             <label for="categories">Categories</label><br>
             <select name="categories[]" id="categories" multiple class="form-control"></select><br><br>
             
             <label for="stock">Stock</label><br>
-            <input type="number" class="form-control" id="stock" name="stock" min=0 value=0>
+            <input type="number" value="{{old('stock')}}" class="form-control {{$errors->first('stock') ? "is-invalid" : ""}} " id="stock" name="stock" min=0 value=0>
+            <div class="invalid-feedback">
+                {{$errors->first('stock')}}
+            </div>
             <br>
             
             <label for="author">Author</label><br>
-            <input type="text" class="form-control" name="author" id="author" placeholder="Book author">
+            <input type="text" value="{{old('author')}}" class="form-control {{$errors->first('author') ? "is-invalid" : ""}} " name="author" id="author" placeholder="Book author">
+            <div class="invalid-feedback">
+                {{$errors->first('author')}}
+            </div>
             <br>
             
             <label for="publisher">Publisher</label> <br>
-            <input type="text" class="form-control" id="publisher" name="publisher" placeholder="Book publisher">
+            <input type="text" value="{{old('publisher')}}" class="form-control {{$errors->first('publisher') ? "is-invalid" : ""}} " id="publisher" name="publisher" placeholder="Book publisher">
+            <div class="invalid-feedback">
+                {{$errors->first('publisher')}}
+            </div>
             <br>
             
             <label for="Price">Price</label> <br>
-            <input type="number" class="form-control" name="price" id="price" placeholder="Book price">
+            <input type="number" value="{{old('price')}}" class="form-control" name="price" id="price" placeholder="Book price" class="form-control {{$errors->first('price') ? "is-invalid" : ""}} ">
+            <div class="invalid-feedback">
+                {{$errors->first('price')}}
+            </div>  
             <br>
             
             <button class="btn btn-primary" name="save_action" value="PUBLISH">Publish</button>
